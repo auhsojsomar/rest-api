66ccfa9eec75c788f886a6b43ca8bbf11f634fb4
Joshua Ramos <auhsojsomar@gmail.com>
2021-02-18 16:38:14 +0800

    commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv/config')
 
 const port = process.env.port || 3000;
 
 app.listen(port, console.log(`Server running on port ${port}`));
 
+mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true,useUnifiedTopology: true }, () => console.log('Connected to MongoDB'));
